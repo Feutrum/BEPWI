808a9b0277c026909f56a68426bfc2dea3c6e4c6
jan_hsm <jan.zone.husemann@gmail.com>
2025-08-25 11:59:57 +0200

    Fahrer wurde für die Fahrzeugreservierung angelegt.

--- src/api/fahrer/controllers/fahrer.js
+++ src/api/fahrer/controllers/fahrer.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::fahrer.fahrer');
+module.exports = createCoreController('api::fahrer.fahrer', ({ strapi }) => ({
+    async available(ctx) {
+        try {
+            const fahrer = await strapi.entityService.findMany('api::fahrer.fahrer', {
+                filters: { verfuegbar: true },
+                populate: ['fahrzeugreservierungs'], // Relationen gleich mitladen
+            });
+
+            return fahrer;
+        } catch (err) {
+            ctx.throw(500, err);
+        }
+    },
+}));
+
