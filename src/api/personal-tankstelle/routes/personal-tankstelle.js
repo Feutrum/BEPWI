9ccfe682d3edf01ac35faa023a37d2403d21c2c1
jan_hsm <jan.zone.husemann@gmail.com>
2025-09-11 17:59:02 +0200

    Neue Version vom Fuhrpark

--- src/api/personal-tankstelle/routes/personal-tankstelle.js
+++ src/api/personal-tankstelle/routes/personal-tankstelle.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * personal-tankstelle router
- */
-
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::personal-tankstelle.personal-tankstelle');
