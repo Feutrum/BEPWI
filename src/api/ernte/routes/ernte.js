f505ec95e0dc1382b6ffeb725afca627c8991975
FighanSuliman <fighan.suliman@stud.th-owl.de>
2025-09-11 20:07:25 +0000

    feat(ernte-formular): implementiere GET-Endpoint für Tätigkeitsspezifisches Formular Ernte

---
+++ src/api/ernte/routes/ernte.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ernte router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ernte.ernte');
---
+++ src/api/ernte/services/ernte.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ernte service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ernte.ernte');
