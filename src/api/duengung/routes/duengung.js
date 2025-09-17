640db04f69e005cc433e52f74a4c0d8cb89d999e
FighanSuliman <fighan.suliman@stud.th-owl.de>
2025-09-17 17:40:56 +0200

    Neues API 'duengung' hinzugefügt

---
+++ src/api/duengung/routes/duengung.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * duengung router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::duengung.duengung');
---
+++ src/api/duengung/services/duengung.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * duengung service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::duengung.duengung');
